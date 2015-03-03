b1e7719c12e0d528ef30560282412ad6ef3fa81f
Ian Westrope <west2737@vandals.uidaho.edu>
2015-03-03 11:58:54 -0800

    Almost done

---
+++ Assignment_3/ksat.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+
+#include "rand.h"
+
+using namespace std;
+
+vector<unsigned long long int> clause;
+vector<unsigned long long int> nMask;
+int n , k , c;
+
+unsigned long long int tourn( unsigned long long int a, unsigned long long int b,
+			      unsigned long long int c );
+
+int fitness( unsigned long long int c );
+unsigned long long int xover( unsigned long long int a, unsigned long long int b );
+unsigned long long int mutate( unsigned long long int a );
+
+int main( int argc, char* argv[] ) {
+
+  if( argc != 4 ) {
+    cout << "Three arguments required" << endl;
+    exit ( -1 );
+  }
+
+  int numGenerations = atoi( argv[1] );
+  int popSize = atoi( argv[2] );
+  double mutateProb = atof( argv[3] );
+
+
+  vector<unsigned long long int> population;
+
+  cin >> n >> k >> c;
+
+  unsigned long long int tmp1, tmp2, mask;
+  for( int i = 0; i < c; i++ ) {
+    cin >> tmp1 >> tmp2;
+    clause.push_back(tmp1);
+    nMask.push_back(tmp2);
+  }
+
+  initRand();
+  mask = (1<<k) -1;
+  
+  for( int i = 0; i < popSize; i++ ) {
+    tmp1 = randULL();
+    tmp1 = tmp1 & mask;
+    population.push_back(tmp1);
+  }
+
+
+  for( int i = 0; i < numGenerations; i++ ) {
+
+    // get 2 elite and stick them in newpopulation
+    int e1 = 0, e2 = 1, tmp1 = 0 , tmp2 = 0, fit;
+    for( int j = 0; j < popSize; j++ ) {
+      fit = fitness( population[j] );
+      if( fit > tmp1 && fit > tmp2 && tmp1 >= tmp2) {
+	tmp2 = fit;
+	e2 = j;
+      }
+      else if( fit > tmp1 && fit > tmp2 && tm1 <= tmp2) {
+	tmp1 = fit;
+	e1 = j;
+      }
+      else if( fit > tmp1 ) {
+	tmp1 = fit;
+	e1 = j;
+      }
+      else if( fit > tmp2 ) {
+	tmp2 = fit;
+	e2 = j;
+      }
+    }
+    vector<unsigned long long int> newpopulation;
+    newpopulation.push_back( population[e1] );
+    newpopulation.push_back( population[e2] );
+
+    
+    // randomly select 3 and put them in a tourny and put winner in new population until it is full
+    int rand1, rand2, rand3;
+    for( int k = 2; k < popSize; k++ ){
+      rand1 = randMod( popSize );
+      rand2 = randMod( popSize );
+      rand3 = randMod( popSize );
+      newpopulation.push_back(tourn( population[rand1], population[rand2], population[rand3] ));
+    }
+
+    // pick 2 from new population and xover into population until pop is full with new
+
+    
+    // mutate pop
+    // put new 
+    
+  }
+  
+}
+
+unsigned long long int mutate( unsigned long long int a ){
+  a = a ^ ( ((randULL() & randULL()) & randULL()) & randULL() );
+  return a;
+}
+
+unsigned long long int xover( unsigned long long int a, unsigned long long int b ) {
+  unsigned long long int mask, child, mask2;
+  
+  for( int i = 1; i <= n; i++ ) {
+    mask = (1<<i) - 1;
+    if( choose( .5 ) ) {
+      mask2 = a & mask;
+    }
+    else {
+      mask2 = b & mask;
+    }
+  }
+  return mask2;
+  
+}
+
+unsigned long long int tourn( unsigned long long int a, unsigned long long int b,
+			      unsigned long long int c ) {
+  int fit1, fit2, fit3;
+
+  fit1 = fitness( a );
+  fit2 = fitness( b );
+  fit3 = fitness( c );
+
+  if( fit1 >= fit2 && fit1 >= fit3 ) { return a; }
+  if( fit2 >= fit1 && fit2 >= fit3 ) { return b; }
+  if( fit3 >= fit2 && fit3 >= fit1 ) { return c; }
+ 
+}
+
+int fitness( unsigned long long int c ){
+  int fit = 0;
+
+  for( int i = 0; i < clause.size(); i++ ) {
+    if( ((c & clause[i]) ^ nMask[i]) != 0 ) {
+      fit++;
+    }
+  }
+
+  return fit;
+}
---
+++ Assignment_3/rand.h
@@ -0,0 +1,37 @@
+#ifndef RANDFLEAH
+#define RANDFLEAH
+
+///////////////////////////////////////////////////
+//
+// Simple fast portable 64 bit random number generators based on
+// a RNG called flea by Bob Jenkins.  Thanks, Bob!
+//
+// http://burtleburtle.net/bob/rand/talksmall.html
+//
+// WARNING: INIT REQUIRED before use!!!
+//
+
+// WARNING: the following includes may vary by OS, since these
+// libraries may be in different places on different machines.
+//#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
+#include <math.h>
+
+void     initRand();                       // init required before use!!!
+void     initRand(unsigned long long int a, unsigned long long int b);  // init with 2 seeds
+
+unsigned long long int randULL();          // 64 bits random number
+double   randUnit();                       // random [0,1]
+double   randPMUnit();                     // random [-1,1]
+int      randMod(int m);                   // random [0,m-1]
+void     randMod2(int m, int &a, int &b);
+int      randMask(unsigned long long int mask);  // random in bit mask
+bool     choose(double prob);              // true with probability prob
+unsigned long long randCoinToss();         // 50:50 true or false
+bool     choose8(int eigth);               // fast choose based on prob in 1/8 increments
+bool     chooseMask(unsigned long long int mask, int prob); 
+double   randNorm(double stddev);          // normal distribution
+double   randCauchy();                     // Cauchy distribution
+double   randCauchy(double mean, double scale);
+#endif
