dcce958b6c1fa83c43579004f7d06640df994823
Ian Westrope <west2737@vandals.uidaho.edu>
2015-03-30 13:15:39 -0700

    Read in input and freq.txt

--- Assignment_4/main.cpp
+++ Assignment_4/main.cpp
@@ -4,15 +4,40 @@
 #include <bitset>
 #include <cstring>
 #include <string>
+#include <fstream>
 
 #include "rand.h"
 
 using namespace std;
 
 int main() {
-  string cipher;
+  string cipher, tmp;
 
-  getline(cin, cipher);
-  cout << cipher << endl;
+  while( getline( cin, tmp ) )
+    {
+      cipher.append( tmp );
+    }
+  //  cout << cipher << endl;
 
+  ifstream freq;
+  freq.open( "freq.txt", ios::in );
+
+  string freqletter[488];
+  int freqnumber[488];
+
+  if( freq.is_open() ) {
+    for(int i = 0; i < 488; i++ ) {
+      freq >> freqletter[i];
+      freq >> freqnumber[i];
+    }
+  }
+
+  freq.close();
+
+  // for(int i = 0; i < 488; i++ ) {
+  //   cout << freqletter[i] << " " << freqnumber[i] << endl;
+  // }
+
+
+  
 }
