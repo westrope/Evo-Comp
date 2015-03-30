38a15b20dae0156226ffbd71e235fd448d067822
Ian Westrope <west2737@vandals.uidaho.edu>
2015-03-30 12:46:45 -0700

    Read in data

---
+++ Assignment_4/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <bitset>
+#include <cstring>
+#include <string>
+
+#include "rand.h"
+
+using namespace std;
+
+int main() {
+  string cipher;
+
+  getline(cin, cipher);
+  cout << cipher << endl;
+
+}
