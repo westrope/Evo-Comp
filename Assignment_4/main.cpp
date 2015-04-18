de19971dab19726b5b945ce9adc2c46591e5f11f
Ian Westrope <west2737@vandals.uidaho.edu>
2015-04-17 17:00:22 -0700

    Use fscanf to read in file.

--- Assignment_4/main.cpp
+++ Assignment_4/main.cpp
@@ -1,3 +1,58 @@
 #include <iostream>
-#include <cstlib>
+#include <cstdlib>
 #include <vector>
+#include <bitset>
+#include <cstring>
+#include <string>
+#include <fstream>
+#include <cstdio>
+
+#include "rand.h"
+
+using namespace std;
+
+class individual {
+public:
+  int key[26];
+  float fit;
+  
+};
+
+// user functions go here
+
+
+int main() {
+
+
+  // get cipher from cin
+  string cipher, tmp;
+  while( getline( cin, tmp ) ) {
+    cipher.append( tmp );
+  }
+
+  // read in freq.txt
+  FILE *freq = fopen( "freq.txt", "r" );
+  char eng1[488], eng2[488];
+  float engnum[488];
+
+  // check if file is open
+  if( freq != NULL ) {
+    for( int i = 0; i < 488; i++ ) {
+      fscanf( freq, "%c%c %f ", &eng1[i], &eng2[i], &engnum[i] );
+      //      eng1[i] = fgetc( freq );
+      //eng2[i] = fgetc( freq );
+   
+      cout << eng1[i] << eng2[i] << " " << engnum[i] <<endl;
+    }
+  }
+  
+
+  // create cipher count tables
+  char cipher1[488], cipher2[488];
+  float ciphernum[488];
+
+  for( int i = 0; i < cipher.size(); i++ ){
+    
+  }
+
+} // end main
